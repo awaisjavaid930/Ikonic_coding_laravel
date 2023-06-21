f7df7381e0f8d148e9e7a3d918489ee93a534e2c
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2023-06-21 11:01:43 +0500

    Database, seeder, factory and model done

---
+++ database/factories/RequestFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class RequestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'sender_id' =>  rand(1, 10),
+            'receiver_id' =>  rand(1, 10),
+            'status' => Arr::random(['pending', 'approved']),
+        ];
+    }
+}
