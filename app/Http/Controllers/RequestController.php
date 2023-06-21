d4a2b9bd198033c6e23c13f24f3c9f3e824d8fb5
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2023-06-21 12:42:28 +0500

    Request Get Pending, Approved, and Decline request Module Done

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\RequestResource;
+use App\Http\Resources\UserResource;
+use App\Models\Request as ConnectionRequst;
+use Auth;
+use App\Models\User;
 
 class RequestController extends Controller
 {
-    //
+    public function suggestedRequest()
+    {
+        $senderId = ConnectionRequst::pluck('sender_id')->toArray();
+        $receiverId = ConnectionRequst::pluck('receiver_id')->toArray();
+        $userIds = array_unique(array_merge($senderId, $receiverId));
+        $users = User::whereNotIn('id',$userIds)->get();
+        return response()->json(['status' => 200 , 'data' => UserResource::collection($users) ]);                
+    
+    }
+    
+    public function pendingRequest()
+    {
+        $pendingRequests = ConnectionRequst::whereStatus('pending')
+                            ->whereReceiverId(Auth::id())->get();
+        return response()->json(['status' => 200 , 'data' => RequestResource::collection($pendingRequests) ]);
+    }
+    
+    public function approvedRequest(ConnectionRequst $request)
+    {
+        $request->update(['status' => 'approved']);
+        return response()->json(['status' => 201 , 'message' => 'request approved!' ]);
+    }
+    
+    public function withdrawRequest(ConnectionRequst $request)
+    {
+        $request->delete();
+        return response()->json(['status' => 201 , 'message' => 'request withdraw!' ]);
+    }
+    
+    public function getApprovedRequest()
+    {
+        $approvedRequests = ConnectionRequst::whereStatus('approved')
+                            ->whereReceiverId(Auth::id())->get();
+        return response()->json(['status' => 200 , 'data' => RequestResource::collection($approvedRequests) ]);                
+    }
+
+    
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+
+    Route::get('/suggested-request', [RequestController::class, 'suggestedRequest']); // suggested request
+    Route::get('/pending-request', [RequestController::class, 'pendingRequest']); // pending request
+    Route::post('/approved-request/{request}', [RequestController::class, 'approvedRequest']); // set approved 
+    Route::post('/withdraw-request/{request}', [RequestController::class, 'withdrawRequest']); // set withdraw request 
+    Route::get('/approved-request', [RequestController::class, 'getApprovedRequest']); // get approved request
+});
